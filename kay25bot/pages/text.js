1c045344eb96502fabd30ebe1c036955f89a375f
ragelorde <phoenixkayce@gmail.com>
2024-12-13 13:58:09 -0800

    Initial commit

---
+++ kay25bot/pages/text.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const Home = () => {
+    return (
+      <div className= 'min-h-screen bg-gradient-to-b from-blue-sea to-purple'>
+        <div className='bg-white/20 rounded-xl p-4 nb-8 text-center'>
+             <div>Points</div>
+             <div>0</div>
+        </div>
+
+        <div>
+            <div>Diamond</div>
+            <div>Tap!</div>
+        </div>
+        <div>Tap the bottom to earn points!</div>
+
+      </div>
+    )
+
+    
+    
+         
+    
+
+}
